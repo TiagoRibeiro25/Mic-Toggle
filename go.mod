067b197ef04e502acb21f70c45e93c536dc1cf5b
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2025-12-18 17:38:46 +0000

    Enforce single instance using Windows mutex
    Added a named mutex using golang.org/x/sys/windows to ensure only one instance of the application runs at a time. Updated dependencies in go.mod to include golang.org/x/sys.

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/moutend/go-hook v0.1.0
 	github.com/moutend/go-wca v0.3.0
 	github.com/wailsapp/wails/v2 v2.11.0
+	golang.org/x/sys v0.39.0
 )
 
 require (
@@ -44,7 +45,6 @@ require (
 	github.com/wailsapp/mimetype v1.4.1 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sys v0.39.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 )
 
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	"os"
+	"syscall"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"golang.org/x/sys/windows"
 )
 
 //go:embed all:frontend/dist
@@ -14,11 +18,25 @@ var assets embed.FS
 var icon []byte
 
 func main() {
+	// Named mutex to allow only one instance
+	mutexName := "MicToggleSingletonMutex"
+	mutex, err := windows.CreateMutex(nil, false, syscall.StringToUTF16Ptr(mutexName))
+	if err != nil {
+		fmt.Println("Error creating mutex:", err)
+		os.Exit(1)
+	}
+	lastErr := windows.GetLastError()
+	if lastErr == windows.ERROR_ALREADY_EXISTS {
+		fmt.Println("Another instance is already running. Exiting...")
+		os.Exit(0)
+	}
+	defer windows.ReleaseMutex(mutex)
+
 	// Create an instance of the app structure
 	app := NewApp()
 
 	// Create application with options
-	err := wails.Run(&options.App{
+	err = wails.Run(&options.App{
 		Title:            "Mic Toggle",
 		Width:            340,
 		Height:           500,
